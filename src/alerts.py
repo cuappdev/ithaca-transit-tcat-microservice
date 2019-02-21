1a25c03c5b53dfd488162260c1d861b268eba7ee
asz33@cornell.edu <asz33@cornell.edu>
2019-02-21 13:47:47 -0500

    Integrate tweeting alerts

--- src/alerts.py
+++ src/alerts.py
@@ -5,17 +5,20 @@ import traceback
 import requests
 
 from src.auth import fetch_auth_header
+from src.twitter import TwitterAPI
 
 ALERTS_URL = 'https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/PublicMessages/GetAllMessages'
+DATE_STRING_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 ONE_MIN_IN_SEC = 60
 
 alerts_data = None
+twitter_api = TwitterAPI()
 
-def convert_to_date(date_str):
+def format_date(date_str):
   # ex) date_str is "/Date(1547528400000-0500)/"
   timestamp = int(date_str[6:date_str.index('-')]) / 1000.0
   utc_date = datetime.utcfromtimestamp(timestamp)
-  return utc_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+  return utc_date.strftime(DATE_STRING_FORMAT)
 
 def convert_num_to_str(tcat_num):
   num_day_dict = {
@@ -33,7 +36,7 @@ def convert_num_to_str(tcat_num):
   return num_day_dict.get(tcat_num, '')
 
 def fetch_alerts(event):
-  global alerts_data 
+  global alerts_data
   try:
     auth_header = fetch_auth_header() 
     headers = {
@@ -44,19 +47,20 @@ def fetch_alerts(event):
     alerts_data = []
     for alert_dict in rq.json():
       alert = {
-          'channelMessages': alert_dict.get('ChannelMessages'),
-          'daysOfWeek': convert_num_to_str(alert_dict.get('DaysOfWeek')),
-          'fromDate': convert_to_date(alert_dict.get('FromDate')),
-          'fromTime': convert_to_date(alert_dict.get('FromTime')),
-          'id': alert_dict.get('MessageId'),
-          'message': alert_dict.get('Message'),
-          'priority': alert_dict.get('Priority'),
-          'routes': alert_dict.get('Routes'),
-          'signs': alert_dict.get('Signs'),
-          'toDate': convert_to_date(alert_dict.get('ToDate')),
-          'toTime': convert_to_date(alert_dict.get('ToTime'))
+          'channelMessages': alert_dict['ChannelMessages'],
+          'daysOfWeek': convert_num_to_str(alert_dict['DaysOfWeek']),
+          'fromDate': format_date(alert_dict['FromDate']),
+          'fromTime': format_date(alert_dict['FromTime']),
+          'id': alert_dict['MessageId'],
+          'message': alert_dict['Message'],
+          'priority': alert_dict['Priority'],
+          'routes': alert_dict['Routes'],
+          'signs': alert_dict['Signs'],
+          'toDate': format_date(alert_dict['ToDate']),
+          'toTime': format_date(alert_dict['ToTime'])
       }
       alerts_data.append(alert)
+    twitter_api.tweet(alerts_data)
   except:
     print(traceback.format_exc())
   threading.Timer(ONE_MIN_IN_SEC, fetch_alerts, [event]).start()
