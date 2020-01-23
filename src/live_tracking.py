4e82514eb5a862a0b66cedf585cef5810af4e02d
alanna-zhou <alanna.zhou@gmail.com>
2020-01-22 20:11:41 -0500

    Modify live tracking to accept JSON instead of XML format for RTF data (#46)

--- src/live_tracking.py
+++ src/live_tracking.py
@@ -1,18 +1,19 @@
 import threading
 import traceback
 
+import json
 import requests
-import xmltodict
 
 RTF_URL = "https://realtimetcatbus.availtec.com/InfoPoint/GTFS-Realtime.ashx?&Type=TripUpdate&debug=true"
 
 rtf_data = None
 
 
-def parse_xml_to_json(xml):
-    ret = xmltodict.parse(xml).popitem()[1]
+def parse_json(json_rtf):
+    ret = json.loads(json_rtf)
 
-    entities = ret["Entities"].popitem()[1]
+    entities = ret["Entities"]
+
     entity_dict = {}
 
     timestamp = ret["Header"]["Timestamp"]
@@ -23,32 +24,25 @@ def parse_xml_to_json(xml):
         trip_update = entity["TripUpdate"]
 
         vehicle_id = None
-        if "Vehicle" in trip_update:
+        if trip_update.get("Vehicle"):
             vehicle_id = trip_update["Vehicle"]["Id"]
 
         route_id = trip_update["Trip"]["RouteId"]
-        delay = trip_update["Delay"]
 
         stop_updates = {}
-        for k in trip_update["StopTimeUpdates"]:
-            stop_time_updates = trip_update["StopTimeUpdates"][k]
 
+        for stop_time_updates in trip_update["StopTimeUpdates"]:
             if isinstance(stop_time_updates, type([])):
                 for stop_update in stop_time_updates:
                     if stop_update["schedule_relationship"] == "NoData":
                         continue
                     stop_id = stop_update["StopId"]
                     stop_updates[stop_id] = stop_update["Arrival"]["Delay"]
-            elif stop_time_updates["schedule_relationship"] != "NoData":
+            elif stop_time_updates["schedule_relationship"] != "NoData" and stop_time_updates["Arrival"]:
                 stop_id = stop_time_updates["StopId"]
                 stop_updates[stop_id] = stop_time_updates["Arrival"]["Delay"]
 
-        entity_dict[entity_id] = {
-            "delay": delay,
-            "routeId": route_id,
-            "stopUpdates": stop_updates,
-            "vehicleId": vehicle_id,
-        }
+        entity_dict[entity_id] = {"routeId": route_id, "stopUpdates": stop_updates, "vehicleId": vehicle_id}
     return entity_dict
 
 
@@ -56,7 +50,7 @@ def fetch_rtf(event):
     global rtf_data
     try:
         rq = requests.get(RTF_URL, headers={"Cache-Control": "no-cache"}, timeout=3)
-        rtf_data = parse_xml_to_json(rq.text)
+        rtf_data = parse_json(rq.text)
     except:
         print(traceback.format_exc())
     threading.Timer(30, fetch_rtf, [event]).start()
