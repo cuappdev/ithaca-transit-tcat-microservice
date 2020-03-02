6baa5071d439b656309abaad6b4d7155d21c3189
alanna-zhou <alanna.zhou@gmail.com>
2020-03-02 14:59:43 -0500

    Add protobuf parse for live tracking (#48)

--- src/live_tracking.py
+++ src/live_tracking.py
@@ -1,56 +1,38 @@
 import threading
 import traceback
 
-import json
-import requests
+import gtfs_realtime_pb2
+import urllib.request
 
-RTF_URL = "https://realtimetcatbus.availtec.com/InfoPoint/GTFS-Realtime.ashx?&Type=TripUpdate&debug=true"
+RTF_URL = "https://realtimetcatbus.availtec.com/InfoPoint/GTFS-Realtime.ashx?&Type=TripUpdate"
 
 rtf_data = None
 
 
-def parse_json(json_rtf):
-    ret = json.loads(json_rtf)
-
-    entities = ret["Entities"]
-
+def parse_protobuf(rq):
     entity_dict = {}
-
-    timestamp = ret["Header"]["Timestamp"]
-    entity_dict["Timestamp"] = timestamp
-
-    for entity in entities:
-        entity_id = entity["Id"]
-        trip_update = entity["TripUpdate"]
-
-        vehicle_id = None
-        if trip_update.get("Vehicle"):
-            vehicle_id = trip_update["Vehicle"]["Id"]
-
-        route_id = trip_update["Trip"]["RouteId"]
-
-        stop_updates = {}
-
-        for stop_time_updates in trip_update["StopTimeUpdates"]:
-            if isinstance(stop_time_updates, type([])):
-                for stop_update in stop_time_updates:
-                    if stop_update["schedule_relationship"] == "NoData":
-                        continue
-                    stop_id = stop_update["StopId"]
-                    stop_updates[stop_id] = stop_update["Arrival"]["Delay"]
-            elif stop_time_updates["schedule_relationship"] != "NoData" and stop_time_updates["Arrival"]:
-                stop_id = stop_time_updates["StopId"]
-                stop_updates[stop_id] = stop_time_updates["Arrival"]["Delay"]
-
-        entity_dict[entity_id] = {"routeId": route_id, "stopUpdates": stop_updates, "vehicleId": vehicle_id}
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(rq.read())
+    for entity in feed.entity:
+        if entity.HasField("trip_update"):
+            if entity.trip_update.HasField("vehicle"):
+                vehicle_id = entity.trip_update.vehicle.id
+            route_id = entity.trip_update.trip.route_id
+            stop_updates = {}
+            for stop_update in entity.trip_update.stop_time_update:
+                if stop_update.schedule_relationship == "NoData":
+                    continue
+                stop_id = stop_update.stop_id
+                stop_updates[stop_id] = stop_update.arrival.delay
+        entity_dict[entity.id] = {"routeId": route_id, "stopUpdates": stop_updates, "vehicleId": vehicle_id}
     return entity_dict
 
 
 def fetch_rtf(event):
     global rtf_data
     try:
-        rq = requests.get(RTF_URL, headers={"Cache-Control": "no-cache"}, timeout=3)
-        rtf_data = parse_json(rq.text)
+        rq = urllib.request.urlopen(RTF_URL)
+        rtf_data = parse_protobuf(rq)
     except:
         print(traceback.format_exc())
     threading.Timer(30, fetch_rtf, [event]).start()
