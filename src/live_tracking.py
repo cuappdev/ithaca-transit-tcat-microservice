10ccc11ef7fce5aec9335734346331a820455cd1
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-10-18 20:44:07 -0400

    addressing comments

--- src/live_tracking.py
+++ src/live_tracking.py
@@ -57,7 +57,7 @@ def add_delay(trip,deviceToken):
         notif_requests[trip] = [deviceToken]
 
     save_notifs(notif_requests)
-    send_notifs()  
+    
     
 #iterates through ths tripIds in the rtf_data to see if any of the delayed
 #tripIds have user's waiting to be notified
@@ -66,16 +66,12 @@ def send_notifs():
     for id in rtf_data:
         if id in notif_requests:
             for user in notif_requests[id]:
-                print(user)
                 #sends a notification to a device if it is waiting for a delay
                 #notification
                 send_notif({'deviceToken':user, 'routeID':rtf_data[id]['routeId']})
             #deletes this tripId from notif_requests as it is no longer needed
-            print('here')
             del notif_requests[id]
             
-    print(notif_requests)
-    print('yay')
     save_notifs(notif_requests)
  
 def save_notifs(notif_requests):
@@ -89,9 +85,6 @@ def send_notif(data):
     url = 'http://127.0.0.1:3000/api/v1/microserviceNotif'
     headers = { 'Content-Type': 'application/json' }
     response = requests.post(url, json=data, headers=headers)
-    print(response)
-    print(data)
-    print('hi')
 
 
 def get_rtf_data():
