899a30d64ede4769eb7430676e7edc292e1ef8a6
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-10-18 20:46:56 -0400

    changed route to dev server

--- src/live_tracking.py
+++ src/live_tracking.py
@@ -82,7 +82,7 @@ def start_notif_timer():
     threading.Timer(30, send_notifs).start()
 
 def send_notif(data):
-    url = 'http://127.0.0.1:3000/api/v1/microserviceNotif'
+    url = 'http://transit-testflight.cornellappdev.com:3000/api/v1/microserviceNotif'
     headers = { 'Content-Type': 'application/json' }
     response = requests.post(url, json=data, headers=headers)
 
